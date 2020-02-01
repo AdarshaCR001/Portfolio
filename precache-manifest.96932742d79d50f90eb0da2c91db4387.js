d78d3c216d0b50151d8bf855f56454df6465a867
Adarsha <adarshacr@outlook.com>
2020-02-01 13:38:10 +0530

    Updates

---
+++ precache-manifest.96932742d79d50f90eb0da2c91db4387.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "6c0453373e08460ed805",
+    "url": "/Portfolio/static/js/main.6c045337.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "6c0453373e08460ed805",
+    "url": "/Portfolio/static/css/main.376d6587.chunk.css"
+  },
+  {
+    "revision": "6c27c7479ed240272a64f47ab4d7e6ea",
+    "url": "/Portfolio/index.html"
+  }
+];
