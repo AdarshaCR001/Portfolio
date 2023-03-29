c2787629ee00b61dc5101c96bc2f773abc3e3201
Adarsha <adarshacr001@gmail.com>
2023-03-29 22:59:12 +0530

    Updates

---
+++ precache-manifest.39814f4452fea1183e1b6010292e0eaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d1ed55f71c6b87e0029f26ee718069",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "e804876e0b870286bacc",
+    "url": "/Portfolio/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "c9c53c6b908129d171d2",
+    "url": "/Portfolio/static/js/2.2acc826c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Portfolio/static/js/2.2acc826c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e804876e0b870286bacc",
+    "url": "/Portfolio/static/js/main.55388cd8.chunk.js"
+  },
+  {
+    "revision": "fb98ab8b5a104bd9298b",
+    "url": "/Portfolio/static/js/runtime-main.f6a8b319.js"
+  }
+]);
